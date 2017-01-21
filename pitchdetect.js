fed8a7c89b06bdc03426b45b691db4d851037ca5
Mason Reed <machreed@ucsc.edu>
2017-01-20 22:32:57 -0800

    DSFGHJH

--- pitchdetect.js
+++ pitchdetect.js
@@ -24,7 +24,7 @@ window.onload = function() {
 };
 
 function getPitch(){
-	return currentPitch;
+	return (currentPitch/2)+100;
 }
 
 function error() {
--- waves.js
+++ waves.js
@@ -1,20 +1,69 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 
-var h = 10;
+var mouseMode = true;
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+  var mouseY
+  canvas.addEventListener('mousemove', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+	mouseY = mousePos.y
+    //console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+  }, false);
+
+var h = 0;
+var haveDebris = false;
+var debrypos = 100;
+var modY;
+
+
 function update(){
 	h = getPitch();
-	console.log(h/2);
+	
+	if(!mouseMode){
+		modY = h;
+	}else{
+		modY = mouseY;
+	}
+	
+	if(haveDebris){
+		debrypos = canvas.height-modY;
+	}else{
+		debrypos = 100;
+	}
+	
+	if(canvas.height-modY < debrypos && !haveDebris){
+		console.log("HIT");
+		haveDebris = true;
+	}
+	
 }
 function draw(){
-
+	canvas.width = canvas.width;
+	canvas.height = canvas.height;
 	ctx.fillStyle = "blue";
-	ctx.rect(0, c.height-h, c.width, h);
+	console.log(canvas.height-modY);
+
 	
-	ctx.fill();
+	ctx.fillRect(0, canvas.height-modY, canvas.width, modY);
+	//ctx.fill();
+
+	
+	ctx.fillStyle = "red";
+	ctx.fillRect(300, debrypos, 100, 100);
+	
+
+		
+	
+	
+	//ctx.fill();
 }
-
-
 
 function game_loop(){
 	update();
