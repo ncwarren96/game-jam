6f7e8ef17ae1a7f9d1f876466ebdaad509f68767
Nick Warren <ncwarren@ucsc.edu>
2017-01-21 18:43:59 -0800

    commitment of a lifetime

--- waves.js
+++ waves.js
@@ -6,10 +6,10 @@ var mouseMode = false;
 var GAME_STATE = 0;
 var LEVEL = 1;
 
-var mouseY = 0;
+var mouseY;
 var pitch;
-var wavePitch = 0;
-var lastpitch = 0;
+var wavePitch;
+var lastpitch;
 
 var haveDebris = false;
 var doUpdate = false;
@@ -20,8 +20,11 @@ var modY;
 var dist = 0;
 var won = false;
 
-var imgBack;
-var bottle, imgBottle;
+var imgBack, imgWave;
+var items;
+var bottle, coconut, beacBall, shell;
+
+var currentItem;
 
 //mouse event listener and getting mouse Y pos
 function getMousePos(canvas, evt) {
@@ -42,39 +45,42 @@ function Items(img, x, y, resistance){
 	this.x = x;
 	this.y = y;
 	this.resistance = resistance;
+	this.next = null;
 	
 	this.draw = function(myx, myy){
 		ctx.drawImage(this.image, myx, myy, this.image.width, this.image.height);
 	};
 }
 
+function imgInit(url, x, y, width, height){
+	var image = new Image();
+	image.x = x;
+	image.y = y;
+	image.width = width;
+	image.height = height;
+	image.src = url;
+	return image;
+}
+
 function init(){
 	GAME_STATE = 0;
 	
-	//initialize background
-	imgBack = new Image();
-	imgBack.x = 0;
-	imgBack.y = 0;
-	imgBack.width = canvas.width;
-	imgBack.height = canvas.height;
-	imgBack.src = "simon/sand.png";
+	imgBack = imgInit("simon/sand.png", 0, 0, canvas.width, canvas.height);
+	imgWave = imgInit("simon/wave.png", 0, 0, canvas.width, canvas.height);
+	var imgBottle = imgInit("simon/bottle.png", 0, 0, 25, 90);
+	var imgCoconut = imgInit("simon/coconut.png", 0, 0, 50, 50);	
+	var imgBeachBall = imgInit("simon/BeachBall.png", 0, 0, 100, 100);
+	var imgShell = imgInit("simon/shell.png", 0, 0, 50, 35);
 	
-	//initialize wave
-	imgWave = new Image();
-	imgWave.x = 0;
-	imgWave.y = 0;
-	imgWave.width = canvas.width;
-	imgWave.height = canvas.height;
-	imgWave.src = "simon/wave.png";
+	//initialize item objects: Image, x-pos, y-pos, resistance
+	bottle = new Items(imgBottle, 100, 200, 20);
+	coconut = new Items(imgCoconut, 100, 200, 20);
+	beachBall = new Items(imgBeachBall, 100, 200, 20);
+	shell = new Items(imgShell, 100, 200, 20);
 	
-	imgBottle = new Image();
-	imgBottle.x = 0;
-	imgBottle.y = 0;
-	imgBottle.width = 25;
-	imgBottle.height = 90;
-	imgBottle.src = "simon/bottle.png";
-	
-	bottle = new Items(imgBottle, 100, 200, 25);
+	bottle.next = coconut;
+	coconut.next = beachBall;
+	beachBall.next = shell;
 	
 	wavePitch = 0;
 	lastpitch = 0;
@@ -83,6 +89,7 @@ function init(){
 	won = false;
 	debrypos = canvas.height -((LEVEL+1)*100);
 	debryposX = Math.random() * ((canvas.width -100) - 100) + 100;
+	
 }
 
 function update(){
@@ -101,8 +108,7 @@ function update(){
 }
 
 function titleScreen(){
-	ctx.font = "30px Arial";
-	ctx.fillText("Click to begin",10,50);
+	currentItem = bottle;
 	
 	canvas.addEventListener('click', function() {
 		GAME_STATE = 1;
@@ -115,9 +121,9 @@ function updatePlay(){
 	wavePitch = getPitch();
 	
 	//pitch logic
-	if(dist>10){
+	if(dist>currentItem.resistance){
 		console.log("speed too fast!");
-		
+
 		if(realDist < 0) {
 			realDist = 10;
 		}else{
@@ -137,13 +143,6 @@ function updatePlay(){
 	}
 	var wavepos = (canvas.height-modY);
 	
-	//constrain wavepos
-	// if(wavepos<0){
-		// wavepos = 0;
-	// }
-	// else if(wavepos>canvas.height){
-		// wavepos = canvas.height;
-	// }
 	
 	//haveDebris is set true if wave reaches y-pos
 	if(wavepos < debrypos-100 && !haveDebris && !won){
@@ -173,7 +172,11 @@ function updatePlay(){
 		init();
 		GAME_STATE = 1;
 		LEVEL++;
-		bottle.image.src = "simon/coconut.png";
+		if(currentItem.next != null){
+			currentItem = currentItem.next;			
+		}
+		//currentItem = currentItem.next;
+		//bottle.image.src = "simon/coconut.png";
 	}
 	
 	lastpitch = wavePitch;
@@ -191,7 +194,7 @@ function draw(){
 	ctx.drawImage(imgWave, 0, canvas.height-modY, imgWave.width, imgWave.height);
 	
 	//item image
-	bottle.draw(debryposX, debrypos);
+	currentItem.draw(debryposX, debrypos);
 	
 	if(won){
 		ctx.font = "72px Arial";
